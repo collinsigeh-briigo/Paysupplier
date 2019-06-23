75c03a2566f623cb9be81655b1e07065fdf81dcb
collinsigeh-briigo <collinsigeh@gmail.com>
2019-06-23 12:53:09 -0700

    code clean-up

--- application/libraries/Custom_library.php
+++ application/libraries/Custom_library.php
@@ -20,117 +20,117 @@ class Custom_library {
     // from a property definition.
     public function __construct()
     {
-            // Assign the CodeIgniter super-object
-            $this->CI =& get_instance();
+        // Assign the CodeIgniter super-object
+        $this->CI =& get_instance();
     }
     
     public function check_login()
     {
-            $this->CI->load->helper('url');    
-            $this->CI->load->library('session');
-            $this->CI->config->item('base_url');
-            if(!isset($_SESSION['user_logged_in']) OR $_SESSION['user_logged_in'] != 'Yes'){
-                redirect(base_url().'dashboard/login/');
-            }
+        $this->CI->load->helper('url');    
+        $this->CI->load->library('session');
+        $this->CI->config->item('base_url');
+        if(!isset($_SESSION['user_logged_in']) OR $_SESSION['user_logged_in'] != 'Yes'){
+        redirect(base_url().'dashboard/login/');
+        }
     }
 
     /**
-         * Checks for and returns account balance in kobo
-         */
-        public function account_balance()
-        {
-                $this->check_login();
+     * Checks for and returns account balance in kobo
+     */
+    public function account_balance()
+    {
+        $this->check_login();
 
-                $balance = 0;
+        $balance = 0;
 
-                $ch = curl_init();
+        $ch = curl_init();
 
-                curl_setopt($ch, CURLOPT_URL, 'https://api.paystack.co/balance');
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                curl_setopt($ch, CURLOPT_HEADER, 0);
-                curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Authorization: Bearer sk_test_964385f725f9e13b9a6579dd8b4dddf460aaf036")); 
+        curl_setopt($ch, CURLOPT_URL, 'https://api.paystack.co/balance');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Authorization: Bearer sk_test_964385f725f9e13b9a6579dd8b4dddf460aaf036")); 
  
-                $output = curl_exec($ch);
+        $output = curl_exec($ch);
 
-                curl_close($ch);
+        curl_close($ch);
 
-                if($output === FALSE){
-                        $_SESSION['errors'] = 'An unexpected error occured.';
-                        redirect(base_url().'dashboard/');
-                }
-
-                $arr = json_decode($output, TRUE);
-
-                $result = $this->extract_array_from_json($arr);
-
-                $result_string = implode("<br>", $result["values"]);
-                $result_array = explode("<br>", $result_string);
-                $balance = $result_array[3];
-
-
-                return $balance;
+        if($output === FALSE){
+            $_SESSION['errors'] = 'An unexpected error occured.';
+            redirect(base_url().'dashboard/');
         }
 
-        /**
-         * Recursive function to extract nested values
-         */
-        public function extract_array_from_json($arr) {
-                global $count;
-                global $values;
-                
-                // Check input is an array
-                if(!is_array($arr)){
-                die("ERROR: Input is not an array");
-                }
-                
-                /*
-                Loop through array, if value is itself an array recursively call the
-                function else add the value found to the output items array,
-                and increment counter by 1 for each value found
-                */
-                foreach($arr as $key=>$value){
-                if(is_array($value)){
-                        $this->extract_array_from_json($value);
-                } else{
-                        $values[] = $value;
-                        $count++;
-                }
-                }
-                
-                // Return total count and values found in array
-                return array('total' => $count, 'values' => $values);
-        }
+        $arr = json_decode($output, TRUE);
 
-        /**
-        * Get and return registered recipients from paystack
+        $result = $this->extract_array_from_json($arr);
+
+        $result_string = implode("<br>", $result["values"]);
+        $result_array = explode("<br>", $result_string);
+        $balance = $result_array[3];
+
+
+        return $balance;
+    }
+
+    /**
+     * Recursive function to extract nested values
+     */
+    public function extract_array_from_json($arr) {
+        global $count;
+        global $values;
+                
+        // Check input is an array
+        if(!is_array($arr)){
+            die("ERROR: Input is not an array");
+        }
+                
+        /*
+        Loop through array, if value is itself an array recursively call the
+        function else add the value found to the output items array,
+        and increment counter by 1 for each value found
         */
-        public function get_suppliers()
-        {
-            $this->check_login();
-
-            $ch = curl_init();
-
-            curl_setopt($ch, CURLOPT_URL, 'https://api.paystack.co/transferrecipient');
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_HEADER, 0);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Authorization: Bearer sk_test_964385f725f9e13b9a6579dd8b4dddf460aaf036")); 
-    
-            $output = curl_exec($ch);
-
-            curl_close($ch);
-
-            if($output === FALSE){
-
-                    $_SESSION['errors'] = 'An unexpected error occured.';
-                    redirect(base_url().'dashboard/');
+        foreach($arr as $key=>$value){
+            if(is_array($value)){
+                $this->extract_array_from_json($value);
+            } else{
+                $values[] = $value;
+                $count++;
             }
+        }
+                
+        // Return total count and values found in array
+        return array('total' => $count, 'values' => $values);
+    }
 
-            $arr = json_decode($output, TRUE);
+    /**
+    * Get and return registered recipients from paystack
+    */
+    public function get_suppliers()
+    {
+        $this->check_login();
 
-            return $arr['data'];
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, 'https://api.paystack.co/transferrecipient');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Authorization: Bearer sk_test_964385f725f9e13b9a6579dd8b4dddf460aaf036")); 
+    
+        $output = curl_exec($ch);
+
+        curl_close($ch);
+
+        if($output === FALSE){
+
+            $_SESSION['errors'] = 'An unexpected error occured.';
+            redirect(base_url().'dashboard/');
         }
 
-        /**
+        $arr = json_decode($output, TRUE);
+
+        return $arr['data'];
+    }
+
+    /**
     * Get and return payouts from paystack api
     */
     public function get_payouts()
